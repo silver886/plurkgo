3a5e941007dc7820677faa6d259f8fbe7df1167a
Leo Liu <karta1041337@gmail.com>
2021-01-25 01:36:56 +0800

    Add type for timeline/getPlurk

--- types/api.go
+++ types/api.go
@@ -5,3 +5,10 @@ type TimelineGetPlurks struct {
 	Users map[int]User `json:"plurk_users"`
 	Posts []Post       `json:"plurks"`
 }
+
+// TimelineGetPlurk is the data responsed by Plurk API 2.0 (/APP/Timeline/getPlurk).
+type TimelineGetPlurk struct {
+	Post  Post         `json:"plurk"`
+	User  User         `json:"user"`
+	Users map[int]User `json:"plurk_users"`
+}
